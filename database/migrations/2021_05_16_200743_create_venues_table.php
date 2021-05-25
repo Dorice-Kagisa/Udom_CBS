080215eaa741ae4f26684ae6694f36b63782ffd3
Dorice-Kagisa <doricejam@gmail.com>
2021-05-25 10:39:12 +0300

    my first test

---
+++ database/migrations/2021_05_16_200743_create_venues_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVenuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('venues', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('venue_name');
+            $table->string('capacity');
+            $table->string('state');
+            $table->point('location');
+            $table->integer('booking_id');
+            $table->string('news');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('venues');
+    }
+}
